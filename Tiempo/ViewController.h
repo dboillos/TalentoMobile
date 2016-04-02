b5ea30f7ec7a9dbb3effdaca2a94796041d48fb2
david <dboillos@yahoo.es>
2016-04-02 12:38:40 +0200

    Initial Commit

---
+++ Tiempo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tiempo
+//
+//  Created by David Boillos on 2/4/16.
+//  Copyright © 2016 David Boillos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tiempo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tiempo
+//
+//  Created by David Boillos on 2/4/16.
+//  Copyright © 2016 David Boillos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
